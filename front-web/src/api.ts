deda6dea4b321ce07290e5a04b4491f5b53ce713
José Milton <j1000tonm.jmb@gmail.com>
2021-01-11 16:41:48 -0300

    Update api mapboxtoken

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
 const API_URL = process.env.REACT_APP_API_URL;
-const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
+const mapboxToken = 'pk.eyJ1IjoiajEwMDB0b25tIiwiYSI6ImNram9tMHczZjEwczEzMWx5bHozZDRsMGkifQ.2X7-ALHTZ5GGndKFowwQEw';
+//const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts() {
     return axios(`${API_URL}/products`)
